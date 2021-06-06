b66a9627f291734bd2b2bb178afa4fa3a5fc9cd3
bromul <mtthw.mldn@gmail.com>
2021-06-06 18:54:12 -0400

    Updating about section

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,16 +1,16 @@
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = d3.select('#selDataset');
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json('samples.json').then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
       selector
-        .append("option")
+        .append('option')
         .text(sample)
-        .property("value", sample);
+        .property('value', sample);
     });
 
     // Use the first sample from the list to build the initial plots
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json('samples.json').then((data) => {
     var metadata = data.metadata;
 
     // Filter the data for the object with the desired sample number
@@ -41,16 +41,16 @@ function buildMetadata(sample) {
     var result = resultArray[0];
     
     // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
+    var PANEL = d3.select('#sample-metadata');
 
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
+    // Use `.html('') to clear any existing metadata
+    PANEL.html('');
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      PANEL.append('h6').text(`${key.toUpperCase()}: ${value}`);
     });
 
   });
@@ -59,7 +59,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json('samples.json').then((data) => {
 
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
